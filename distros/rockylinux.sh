e429eb1490eddcd90abec62671966a6ec60ff4e9
badcakee <antta6487@gmail.com>
2025-07-16 12:53:16 +0200

    Create rockylinux.sh

---
+++ distros/rockylinux.sh
@@ -0,0 +1,6 @@
+pkg update 
+pkg install proot-distro -y 
+proot-distro install rockylinux 
+clear 
+echo ✅ Successfully installed rockylinux. Thank you for using my script :D
+proot-distro login rockylinux
