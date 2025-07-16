2d42dbd8fa28e1f6b16b67e44d5ddf99c9d09485
badcakee <antta6487@gmail.com>
2025-07-16 16:16:09 +0200

    Update trollosinstall.sh

--- distros/trollosinstall.sh
+++ distros/trollosinstall.sh
@@ -4,5 +4,5 @@ pkg update
 pkg install proot-distro -y 
 proot-distro install ubuntu 
 clear 
-echo ✅ Successfully installed Trollos, please run . Thank you for using my script :D
+echo ✅ Successfully installed Trollos, please run bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/trollos.sh). Thank you for using my script :D
 proot-distro login ubuntu
