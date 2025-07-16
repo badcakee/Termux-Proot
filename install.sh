5ac45c25886fe370064acd0f19ca5293b38921f2
badcakee <antta6487@gmail.com>
2025-07-16 14:52:09 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo Installing dependencies
+apt install curl -y
+
+echo "Choose a distro:"
+echo "1) Debian"
+echo "2) Ubuntu"
+echo "3) Adelie"
+echo "4) Alpine"
+echo "5) Archlinux"
+echo "6) Chimera"
+echo "7) Deepin"
+echo "8) Fedora"
+echo "9) Manjaro"
+echo "10) Opensuse"
+echo "11) Pardus"
+echo "12) Rockylinux"
+echo "13) Void"
+echo "14) Bados (soon)"
+read -p "Enter a number (1-14): " choice
+
+case $choice in
+  1) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/debian.sh) ;;
+  2) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/ubuntu.sh) ;;
+  3) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/adelie.sh) ;;
+  4) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/alpine.sh) ;;
+  5) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/archlinux.sh) ;;
+  6) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/chimera.sh) ;;
+  7) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/deepin.sh) ;;
+  8) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/fedora.sh) ;;
+  9) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/manjaro.sh) ;;
+  10) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/opensuse.sh) ;;
+  11) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/pardus.sh) ;;
+  12) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/rockylinux.sh) ;;
+  13) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/void.sh) ;;
+  14) echo "Bados is coming soon!" ;;
+  *) echo "Invalid choice. Please enter a number from 1 to 14." ;;
+esac
