bd22a7871e062082716153c2ff9a90cbd3d0f2e7
badcakee <antta6487@gmail.com>
2025-07-16 12:39:23 +0200

    Update debian.sh

--- distros/debian.sh
+++ distros/debian.sh
@@ -2,5 +2,5 @@ pkg update
 pkg install proot-distro -y 
 proot-distro install debian 
 clear 
-echo ✅ Successfully installed debian, Please run apt update. Thank you for using my script :D
+echo ✅ Successfully installed debian. Thank you for using my script :D
 proot-distro login debian
