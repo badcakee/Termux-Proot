c04a8a398ac909628574a722a65cde64ce4226f8
badcakee <antta6487@gmail.com>
2025-07-16 12:48:06 +0200

    Create archlinux.sh

---
+++ distros/archlinux.sh
@@ -0,0 +1,6 @@
+pkg update 
+pkg install proot-distro -y 
+proot-distro install archlinux 
+clear 
+echo ✅ Successfully installed archlinux. Thank you for using my script :D
+proot-distro login archlinux
