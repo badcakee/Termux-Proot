0a024b8a5d9b0215a85d720d5e9428553df46792
badcakee <antta6487@gmail.com>
2025-07-16 15:41:59 +0200

    Update btr.sh

--- btr.sh
+++ btr.sh
@@ -2,8 +2,10 @@
 
 if [ "$1" = "login" ] && [ -n "$2" ]; then
   if proot-distro login "$2"; then
+    clear
     echo "✅ Successfully logged in"
   else
+    clear
     echo "❌ Login failed"
   fi
 else
