832133c4812d50fbb5b138eef9f1094cc71226fe
badcakee <antta6487@gmail.com>
2025-07-16 12:51:59 +0200

    Create opensuse.sh

---
+++ distros/opensuse.sh
@@ -0,0 +1,6 @@
+pkg update 
+pkg install proot-distro -y 
+proot-distro install opensuse 
+clear 
+echo ✅ Successfully installed opensuse. Thank you for using my script :D
+proot-distro login opensuse
