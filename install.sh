f433510da5ab87e068ce66538a3ff8ffb8b75dc8
badcakee <antta6487@gmail.com>
2025-09-17 13:13:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pkg update
 
 echo "Installing BTR..."
 mkdir -p ~/bin
-curl -fsSL https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/btr.sh -o ~/bin/btr
+curl -fsSL https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/btr.sh -o ~/bin/btr
 chmod +x ~/bin/btr
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
 export PATH=$HOME/bin:$PATH
@@ -28,19 +28,19 @@ echo "14) Trollos (soon)"
 read -p "Enter a number (1-14): " choice
 
 case $choice in
-  1) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/debian.sh) ;;
-  2) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/ubuntu.sh) ;;
-  3) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/adelie.sh) ;;
-  4) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/alpine.sh) ;;
-  5) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/archlinux.sh) ;;
-  6) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/chimera.sh) ;;
-  7) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/deepin.sh) ;;
-  8) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/fedora.sh) ;;
-  9) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/manjaro.sh) ;;
-  10) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/opensuse.sh) ;;
-  11) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/pardus.sh) ;;
-  12) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/rockylinux.sh) ;;
-  13) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Root/refs/heads/main/distros/void.sh) ;;
+  1) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/debian.sh) ;;
+  2) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/ubuntu.sh) ;;
+  3) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/adelie.sh) ;;
+  4) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/alpine.sh) ;;
+  5) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/archlinux.sh) ;;
+  6) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/chimera.sh) ;;
+  7) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/deepin.sh) ;;
+  8) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/fedora.sh) ;;
+  9) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/manjaro.sh) ;;
+  10) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/opensuse.sh) ;;
+  11) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/pardus.sh) ;;
+  12) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/rockylinux.sh) ;;
+  13) bash <(curl -s https://raw.githubusercontent.com/badcakee/Termux-Proot/refs/heads/main/distros/void.sh) ;;
   14) echo "doesn't it say soon?" ;;
   *) echo "Invalid choice. Please enter a number from 1 to 14." ;;
 esac
